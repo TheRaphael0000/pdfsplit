635d3b45cb2c024bc425592726a22d78f9bc2989
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-10-21 19:47:42 +0200

    add page order argument

--- multipagespdftosinglepage/__main__.py
+++ multipagespdftosinglepage/__main__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
 import argparse
-from .spliter import split
+from .spliter import split, create_pdf
+
+
+def valid_read_order(o):
+    possible = [
+        "tblr", "tbrl", "btrl", "btlr",
+        "lrtb", "rltb", "rlbt", "lrbt",
+    ]
+
+    if o not in possible:
+        return False
+    return o
 
 
 def main():
@@ -10,15 +21,18 @@ def main():
     parser.add_argument("input", help="Input file")
     parser.add_argument(
         "output", help="Output file (doesn't work with the same as input file)")
-    parser.add_argument('-r', '--rows', type=int,
+    parser.add_argument("-r", "--rows", type=int,
                         default=2, help="Number of rows")
-    parser.add_argument('-c', '--cols', type=int,
+    parser.add_argument("-c", "--cols", type=int,
                         default=2, help="Number of colunms")
+    parser.add_argument("-o", "--order", type=valid_read_order,
+                        default="tblr", help="Reading order : By default Top-Bottom then Left-Right")
 
     args = parser.parse_args()
-    pdf = split(args.input, args.rows, args.cols)
-    pdf.write(open(args.output, 'wb'))
+    pages = split(args.input, args.rows, args.cols, args.order)
+    pdf = create_pdf(pages)
+    pdf.write(open(args.output, "wb"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- multipagespdftosinglepage/spliter.py
+++ multipagespdftosinglepage/spliter.py
@@ -2,9 +2,44 @@ import copy
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
-def split(filename, rows, cols):
-    input = PdfFileReader(open(filename, 'rb'))
+def order_to_index(rows, cols, order):
+    swap_xy = "l" == order[0] or "l" == order[1]
+    if swap_xy:
+        x = order[0:2]
+        y = order[2:4]
+    else:
+        x = order[2:4]
+        y = order[0:2]
+    x_order = 1 if x[0] == "l" else -1
+    y_order = 1 if y[0] == "t" else -1
+    return swap_xy, x_order, y_order
+
+
+def create_pdf(pages):
     output = PdfFileWriter()
+    for page in pages:
+        output.addPage(page)
+    return output
+
+
+def split(filename, rows, cols, order):
+    input = PdfFileReader(open(filename, "rb"))
+    pages = []
+
+    swap_xy, x_order, y_order = order_to_index(rows, cols, order)
+
+    def crop_add_page(subpage_i, subpage_j):
+        x = subpage_i * col_w
+        y = subpage_j * row_h
+        page_copy = copy.copy(page)
+        page_copy.mediaBox = copy.copy(page_copy.mediaBox)
+
+        ll = (x, y)
+        page_copy.mediaBox.setLowerLeft(ll)
+        ur = (x+col_w, y+row_h)
+        page_copy.mediaBox.setUpperRight(ur)
+
+        pages.append(page_copy)
 
     for page_i in range(input.getNumPages()):
         page = input.getPage(page_i)
@@ -13,18 +48,13 @@ def split(filename, rows, cols):
         col_w = w/cols
         row_h = h/rows
 
-        for subpage_i in range(rows):
-            x = subpage_i * col_w
-            for subpage_j in range(cols)[::-1]:
-                y = subpage_j * row_h
-                c = copy.copy(page)
-                c.mediaBox = copy.copy(c.mediaBox)
+        if swap_xy:
+            for subpage_i in range(rows)[::x_order]:
+                for subpage_j in range(cols)[::-y_order]:
+                    crop_add_page(subpage_i, subpage_j)
+        else:
+            for subpage_j in range(cols)[::-y_order]:
+                for subpage_i in range(rows)[::x_order]:
+                    crop_add_page(subpage_i, subpage_j)
 
-                ll = (x, y)
-                c.mediaBox.setLowerLeft(ll)
-                ur = (x+col_w, y+row_h)
-                c.mediaBox.setUpperRight(ur)
-
-                output.addPage(c)
-
-    return output
+    return pages
