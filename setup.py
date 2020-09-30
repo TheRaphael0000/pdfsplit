8a8d250f3071f952fb3ef8a74e405ec7f05da31c
Raphaël Margueron <raphael.margueron@gmail.com>
2020-09-30 14:42:46 +0200

    rename2 the module

--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@ def read(fname):
 
 
 setup(
-    name="splitpdf",
+    name="pdfsplit",
     version="0.0.1",
     author="Raphaël Margueron",
     author_email="raphael.margueron@gmail.com",
     description=(""),
     license="",
     keywords="",
-    url="https://github.com/TheRaphael0000/splitpdf",
-    packages=["splitpdf"],
+    url="https://github.com/TheRaphael0000/pdfsplit",
+    packages=["pdfsplit"],
     long_description=read("README.md"),
     classifiers=[],
     install_requires=read("requirements.txt"),
     entry_points={
         "console_scripts": [
-            "pdfsplit=splitpdf.__main__:main"
+            "pdfsplit=pdfsplit.__main__:main"
         ]
     },
 )
